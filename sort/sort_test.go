2cd1d2123afb2951dc5c8754e1f467a59928bed4
Bill Glover <bill@billglover.co.uk>
2018-01-04 21:43:56 +0000

    Test for nil, empty and single digit slices
    We treat nil, empty and single digit slices as sorted. These tests ensure that this is the case.

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -13,12 +13,24 @@ func TestMergeSort(t *testing.T) {
 		list []int
 	}{
 		{
-			"positive integers in reverse order",
-			[]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+			"nil slice",
+			nil,
+		},
+		{
+			"empty slice",
+			[]int{},
+		},
+		{
+			"single integer",
+			[]int{100},
 		},
 		{
 			"already sorted list",
 			[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+		{
+			"positive integers in reverse order",
+			[]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
 		},
 		{
 			"random initial list",
